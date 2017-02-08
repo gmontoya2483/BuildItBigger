292e73a1155de1aa3e0022380b0fee876cb5c5ac
Gabriel Montoya <gabi.montoya@gmail.com>
2017-02-08 17:56:15 +0100

    Step 5: Add a Paid Flavor - Completed.

---
+++ app/src/free/java/com/example/gabriel/buildbigger/free/MainActivityFragment.java
@@ -0,0 +1,47 @@
+package com.example.gabriel.buildbigger.free;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gabriel.buildbigger.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+/**
+ * Created by Gabriel on 08/02/2017.
+ */
+
+public class MainActivityFragment extends Fragment {
+
+
+
+    public MainActivityFragment() {
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+            AdView mAdView = (AdView) root.findViewById(R.id.adView);
+            // Create an ad request. Check logcat output for the hashed device ID to
+            // get test ads on a physical device. e.g.
+            // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+            AdRequest adRequest = new AdRequest.Builder()
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .build();
+            mAdView.loadAd(adRequest);
+
+        return root;
+    }
+
+
+
+}
--- app/src/main/java/com/example/gabriel/buildbigger/MainActivityFragment.java
+++ app/src/main/java/com/example/gabriel/buildbigger/MainActivityFragment.java
@@ -1,13 +1,12 @@
 package com.example.gabriel.buildbigger;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 /**
  * Created by Gabriel on 08/02/2017.
@@ -15,7 +14,11 @@ import com.google.android.gms.ads.AdView;
 
 public class MainActivityFragment extends Fragment {
 
+
+
     public MainActivityFragment() {
+
+
     }
 
     @Override
@@ -23,14 +26,10 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
+
+
+
+
         return root;
     }
 
